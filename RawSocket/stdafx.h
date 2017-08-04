3e2a63b095dc8f62d6149b1501d9996890c9c824
ShiningRush <277040271@qq.com>
2017-08-04 18:50:30 +0800

    add src

---
+++ RawSocket/stdafx.h
@@ -0,0 +1,39 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <winsock2.h>
+#include <mstcpip.h>
+
+#pragma comment(lib, "WS2_32")	// 链接到WS2_32.lib
+
+class CInitSock
+{
+public:
+	CInitSock(BYTE minorVer = 2, BYTE majorVer = 2)
+	{
+		// 初始化WS2_32.dll
+		WSADATA wsaData;
+		WORD sockVersion = MAKEWORD(minorVer, majorVer);
+		if (::WSAStartup(sockVersion, &wsaData) != 0)
+		{
+			exit(0);
+		}
+	}
+	~CInitSock()
+	{
+		::WSACleanup();
+	}
+};
+
+
+
+
+// TODO: 在此处引用程序需要的其他头文件
